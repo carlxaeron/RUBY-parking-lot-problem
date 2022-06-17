82390ec9639dbc0b6d5802891f09806f256fdfc2
carl louis manuel <carlxaeron@users.noreply.github.com>
2022-06-18 07:18:08 +0800

    Update my_program.rb
    # when command does not have input file argument then default to file_inputs.txt

--- my_program.rb
+++ my_program.rb
@@ -1,5 +1,10 @@
-fileObj = File.open("file_inputs.txt", "r")
-fileObj.each_line do |line|
-        puts line
-    end
-fileObj.close
+def main
+    # when command does not have input file argument then default to file_inputs.txt
+    fileObj = File.open(ARGV[0] || "file_inputs.txt", "r")
+    fileObj.each_line do |line|
+            puts line
+        end
+    fileObj.close
+end
+
+main()
