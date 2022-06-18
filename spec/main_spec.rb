f5bfd8334a79cd46ce0cac57f29cd0e3f9f29311
carl louis manuel <carlxaeron@users.noreply.github.com>
2022-06-18 09:25:43 +0800

    Update main_spec.rb
    create a delete the file after finishing the test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,6 +1,10 @@
 require './my_program.rb'
 
+# create a input file
 filename = rand(1..100)
-output = File.new("#{filename}.txt", "w")
+output = File.new("#{filename.hash}.txt", "w")
 output.puts("test")
 output.close
+
+# delete file
+File.delete("#{filename}.txt")
