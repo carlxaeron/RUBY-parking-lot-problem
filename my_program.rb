74a51479d4ebacc53419ac97fa4dcdedb211a474
carl louis manuel <carlxaeron@users.noreply.github.com>
2022-06-19 12:55:41 +0800

    more description below
    separate the codes from my_program to new file for flexibility

--- my_program.rb
+++ my_program.rb
@@ -1,23 +1,3 @@
-require './my_program/parking_lot'
-Parking = ParkingLot.new
-
-def process_cmd(txt)
-    cmd = txt.split ' '
-    case cmd[0]
-        when "create_parking_lot"
-            Parking.create_parking_lot(cmd[1])
-    end
-end
-
-def main
-    # when command does not have input file argument then default to file_inputs.txt
-    fileObj = File.open(ARGV[0] || "file_inputs.txt", "r")
-    fileObj.each_line do |line|
-            process_cmd(line)
-        end
-    fileObj.close
-
-    puts Parking.inspect
-end
+require './my_program/main'
 
 main()
---
+++ my_program/main.rb
@@ -0,0 +1,21 @@
+require './my_program/parking_lot'
+Parking = ParkingLot.new
+
+def process_cmd(txt)
+    cmd = txt.split ' '
+    case cmd[0]
+        when "create_parking_lot"
+            Parking.create_parking_lot(cmd[1])
+    end
+end
+
+def main(file = nil)
+    # when command does not have input file argument then default to file_inputs.txt
+    fileObj = File.open(file || ARGV[0] || "file_inputs.txt", "r")
+    fileObj.each_line do |line|
+            process_cmd(line)
+        end
+    fileObj.close
+
+    puts Parking.inspect
+end
